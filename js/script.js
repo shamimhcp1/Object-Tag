5aa34940796ee222a2c87d8102806b21e88c172e
shamimhcp1 <shamimhcp1@gmail.com>
2018-05-27 02:29:09 +0600

    object Tagcing

---
+++ js/script.js
@@ -0,0 +1,18 @@
+$('#hCardBtn').click(function () {
+    $('#vCardObject').css({
+        'display' : 'none'
+    })
+    $('#hCardObject').css({
+        'display' : 'block'
+    })
+});
+
+$('#vCardBtn').click(function () {
+    $('#hCardObject').css({
+        'display' : 'none'
+    })
+    $('#vCardObject').css({
+        'display' : 'block'
+    })
+});
+
